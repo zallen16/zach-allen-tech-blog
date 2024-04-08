082b6fb051f54267060dece901262568da58c1d7
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-04-07 23:57:01 -0400

    completed comments route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
-const withAuth = require('../../utils/auth');
+const { Comment } = require('../../models');
+
+router.post('/', async (req, res) => {
+    console.log('blogRoutes post "/comment"');
+    console.log(req.body.blog_id);
+    try {
+        const newComment = await Comment.create({
+            comment_content: req.body.comment,
+            blog_id: req.body.blog_id,
+        });
+
+        console.log(newComment);
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
